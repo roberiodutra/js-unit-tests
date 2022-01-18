7703b797cd059c668a005556a385f8dacaf5700b
Robério Almeida <dev.roberio@gmail.com>
2022-01-18 16:00:46 -0300

    myCounter ok?

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,11 +17,11 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  const myArray = [];
+  for (let i = 0; i <= 3; i += 1) {
+    myArray.push(i);
+    for (let j = 2; j <= 3; j += 1) {
+      myArray.push(j);
     }
   }
   return myArray;
