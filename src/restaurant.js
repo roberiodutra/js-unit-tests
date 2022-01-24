ff935ec4802fe573dfcf083e702c265166dc3f32
Robério Almeida <dev.roberio@gmail.com>
2022-01-24 01:32:37 -0300

    lintão

--- src/restaurant.js
+++ src/restaurant.js
@@ -79,7 +79,7 @@
 // que percorre por todos os itens de `objetoRetornado.consumption`, soma o preço deles e retorna o valor somado acrescido de 10%.
 // DICA: para isso, você precisará percorrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-//Menu
+// Menu
 const menu = {
   coxinha: 3.90, 
   sanduiche: 9.90,
@@ -111,5 +111,6 @@ createMenu.pay = () => {
     return acc;
   }, 0);
   return total + (total * 0.10);
-}
+};
+
 module.exports = createMenu;
